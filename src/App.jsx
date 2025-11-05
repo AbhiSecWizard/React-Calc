1ce02d01f99f29924a33cc39c429dc5ee1dbd3f5
Abhishek yadav <studyandrotech@gmail.com>
2025-11-05 07:20:07 +0530

    layout

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+export const App = () => {
+  return (
+    <div className="app">
+     <div className='app_calculator'>
+        <div className='app_calculator_navbar'>
+          <div className='app_calculator_toggle'>
+            <div className='app_calculator_navbar_toggle '>
+                  
+            </div>
+
+          </div>
+        </div>
+     </div>
+    </div>
+  )
+}
