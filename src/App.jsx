529e093b020fa1a62b663a6649514dbad1bce6a5
Abhishek yadav <studyandrotech@gmail.com>
2025-11-06 09:05:23 +0530

    laout and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-
+import moonIcon from "./assets/moon.png"
+import sunIcon from "./assets/sun.png"
+import {Header} from "./Component/Headers/Header"
+import {KeyPad} from "./Component/KeyPad/KeyPad"
 import './App.css'
+import { useState } from "react"
 export const App = () => {
+  const [isDarkMode,setDarkMode]=useState(false)
   return (
     <div className="app">
      <div className='app_calculator'>
         <div className='app_calculator_navbar'>
           <div className='app_calculator_toggle'>
-            <div className='app_calculator_navbar_toggle '>
-                  
+            <div className='app_calculator_navbar_toggle' 
+            onClick={()=>setDarkMode(!isDarkMode)}
+            >
+                  <div className={`app_calculator_navbar_toggle_circle${isDarkMode ? " app_calculator_navbar_toggle_circle_active" : ""}`}
+>
+  
+                    </div>
             </div>
-
           </div>
+                    <img src={isDarkMode ? moonIcon : sunIcon} alt="mode"/>
+   
         </div>
+      <Header/>
+      <KeyPad/>
      </div>
     </div>
   )
---
+++ src/Component/KeyPad/KeyPad.jsx
@@ -0,0 +1,9 @@
+import "./KeyPad.css"
+
+export const KeyPad=()=>{
+    return (
+        <div className="keypad">
+            <h1>KeyPad</h1>
+        </div>
+    )
+}
